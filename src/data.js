5ef7ca8454c3fb9225e7830baa6d5341488cf973
Ali <aliuajibade317@gmail.com>
2022-08-10 09:36:05 -0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,89 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: `Easy, American-style, fluffy pancakes are great for feeding a crowd at breakfast or brunch. Top with something sweet like fruit, jam or syrup, or rashers of crispy bacon.`,
+  },
+  {
+    id: 2,
+    title: "dinner double",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: `Create a potato hash with a difference. The addition of cabbage ramps up the texture and nutrition, and swap bacon for leaner fish`,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: `A Glasgow comic cafe that specialises in all things geek culture has created two limited edition milkshakes to celebrate the launch of the new Godzilla: King of Monsters film.
+      Geek Retreat at 63 Union Street in Glasgow's city centre created the milkshakes for a limited time - so get them while you still can!`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: `The first chocolate-flavoured milkshake is called Choczilla: King of Milkshakes and is covered with cookies and biscuits - we're sold already!
+      Meanwhile, the electric blue the Atomic Bubblegum comes decorated with plenty of wafers and edible straws so you have something to chow down on while enjoying your drink.`,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: `The first chocolate-flavoured milkshake is called Choczilla: King of Milkshakes and is covered with cookies and biscuits - we're sold already!
+          Meanwhile, the electric blue the Atomic Bubblegum comes decorated with plenty of wafers and edible straws so you have something to chow down on while enjoying your drink.`,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: `Tantalize your taste-buds to this creamy, delish Heavenly Oreo Cookie Cream Layered Dessert! It's sooooo amazing and only has 8 ingredients to make it super easy for you!! `,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-7.jpeg",
+    desc: `Bacon wrapped food is a style of food preparation, where bacon is wrapped around other ingredients or dishes, and either grilled, fried, or baked.
+          Many of the wrapped foods, such as livers and asparagus, cook more quickly than bacon itself does, and when preparing such dishes it is thus necessary to part-cook the bacon separately, before wrapping the filling and cooking the complete dish. Bacon-wrapped foods can include many things including filet mignon, chicken nuggets, pork chops, shrimp, Juicy Lucy, and tenderloin`,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: `How do you get crispy bacon wrapped?
+        You can wrap your food in raw bacon and then cook both together. If the main food is done first you can turn the food on its side so that the bacon is now touching the grate so that you can crisp it up. But make sure and this is important, to turn down that heat to about 375-400.`,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: `As this pandemic rages on, it is easy for some to feel lonely or isolated. A couple of Cornell University students noticed that and decided to use their education to bring people together. They’re calling it “Quarantine Buddy.”`,
+  },
+  {
+    id: 10,
+    title: "quarantine buddy",
+    category: "dinner",
+    price: 99.99,
+    img: "./images/item-9.jpeg",
+    desc: `And so far, over 40,000 people from 137 different countries are on board, or should we say online. Computer science major Sam Brickman along with Jordyn Goldzweig came up with a website in which total strangers, all feeling the anxiety and loneliness of the COVID-19 pandemic, can have a virtual hang out session.`,
+  },
+];
+
+export default menu;
